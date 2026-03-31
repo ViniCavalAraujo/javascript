a829dceebd9e9f97e6efa239ce810f73fb525004
ViniCavalAraujo <viniciuscavaldearaujo@hotmail.com>
2026-03-30 22:19:02 -0300

    condição aninhada

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 15
+if (idade < 16) {
+    console.log('Não pode votar')
+} else {
+    if (idade < 18) {
+        console.log('Voto opcional')
+    } else {
+        console.log('Pode votar')
+    }
+}
