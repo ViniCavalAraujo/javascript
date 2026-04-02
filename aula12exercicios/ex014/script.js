b1a61bfc0f6b55662dacf1cd83319648c13aa0ab
ViniCavalAraujo <viniciuscavaldearaujo@hotmail.com>
2026-04-02 11:12:30 -0300

    ajuste no exercício

--- aula12exercicios/ex014/script.js
+++ aula12exercicios/ex014/script.js
@@ -1,6 +1,6 @@
 function carregar(){
 var msg = window.document.getElementById('msg')
-var img = window.document.querySelector('#imagem img')
+var img = window.document.querySelector('#foto img')
 var data = new Date()
 var hora = data.getHours()
 var minutos = data.getMinutes()
@@ -8,14 +8,16 @@ msg.innerHTML = `Agora são ${hora}:${minutos}`
 if (hora >= 0 && hora < 12) {
     msg.innerHTML = 'BOM DIA'
     img.src = 'imagens/manha.jpg'
+    document.body.style.background = 'yellow'
 
 } else if (hora >= 12 && hora < 18) {
     msg.innerHTML = 'BOA TARDE'
     img.src = 'imagens/tarde.jpg'
-
+    document.body.style.background = 'orange'
 } else {
     msg.innerHTML = 'BOA NOITE'
     img.src = 'imagens/noite.jpg'
+    document.body.style.background = 'black'
 }
 
 }
