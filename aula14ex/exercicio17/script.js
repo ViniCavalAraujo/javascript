53ceb91485ea232011d746cbbb7213d9b41edf88
ViniCavalAraujo <viniciuscavaldearaujo@hotmail.com>
2026-05-13 19:50:29 -0300

    tabuada com for

--- aula14ex/exercicio17/script.js
+++ aula14ex/exercicio17/script.js
@@ -1,1 +1,27 @@
-alert('ola')
+function tabuada() {
+    var num = document.getElementById('num')
+    var numtabuada = Number(num.value)
+    var res = document.getElementById('res')
+
+    res.innerHTML = ''
+
+    if (num.value == '') {
+        alert('Insira um número')
+        return
+    }
+
+    if (numtabuada == 0) {
+        alert('Zero não é multiplicável')
+        return
+    }
+
+    if (numtabuada < 0) {
+        alert('Número inválido')
+        return
+    }
+    for (var i = 1; i <= 10; i++) {
+       res.innerHTML += `${numtabuada}x${i}= ${numtabuada * i}<br>`
+       
+    }
+    
+}
