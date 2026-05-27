f3c45a9cd273377b46b9c9790e95f211ca78158f
ViniCavalAraujo <viniciuscavaldearaujo@hotmail.com>
2026-05-26 21:11:27 -0300

    aprendendo vetor

---
+++ aula15/ambiente.js
@@ -0,0 +1,15 @@
+let num = []
+
+num = [0,5,1]
+num [0] = 10
+num.push(30)
+num.sort((a,b) => b-a)
+
+for (let i in num) {
+    console.log(num[i])
+}
+
+
+let frutas = ['banana', 'abacaxi', 'uva']
+frutas.sort((a,b) => b.localeCompare(a))
+console.log(frutas)
