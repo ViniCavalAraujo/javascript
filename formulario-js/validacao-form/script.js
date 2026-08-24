27398f679918934522409088e7d60d6cb7797ae4
ViniCavalAraujo <viniciuscavaldearaujo@hotmail.com>
2026-08-24 19:02:13 -0300

    validações regex em formulário

---
+++ formulario-js/validacao-form/script.js
@@ -0,0 +1,58 @@
+//captação do formulário
+const form = document.querySelector('#dados-form') 
+
+
+//evento de submit do formulário 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const dados = {
+        nomeUsuario: document.querySelector('#nome').value,
+        emailUsuario: document.querySelector('#email').value,
+        telUsuario: document.querySelector('#telefone').value
+    }
+
+
+    //validações dos inputs
+    if (dados.nomeUsuario === '' || dados.emailUsuario === '' || dados.telUsuario === '') {
+        alert('Preencha todos os campos')
+        return
+    }
+
+    if (!validarNome(dados.nomeUsuario)) {
+        alert('nome inválido')
+        return
+    }
+
+    if (!validarEmail(dados.emailUsuario)) {
+        alert('e-mail inválido')
+        return
+    }
+
+    if (!validarTelefone(dados.telUsuario)) {
+        alert('telefone inválido')
+        return
+    }
+
+    alert('dados enviados')
+    form.reset()
+})
+
+
+//funções regex para verificação dos inputs
+const validarNome = (nome) => {
+    const regexNome = /^[a-zA-ZÀ-ÿ\s]+$/
+    return regexNome.test(nome)
+}
+
+const validarEmail = (email) => {
+    const regexEmail = /^[^\s]+@[^\s]+\.[^\s]+$/
+    return regexEmail.test(email)
+}
+
+const validarTelefone = (telefone) => {
+    const apenasNumeros = telefone.replace(/[^0-9]/g, "")
+    const regexNome = /^[0-9]{10,11}$/
+
+    return regexNome.test(apenasNumeros)
+}
